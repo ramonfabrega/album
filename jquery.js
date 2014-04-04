974a3e1b3c0ec028ff8a146dd7b705156f136f8e
Ramon Fabrega <ramonfabrega0@gmail.com>
2014-04-04 08:38:59 -0500

    Added delete button functionality

--- jquery.js
+++ jquery.js
@@ -2,4 +2,9 @@ $(document).ready(function(){
     $('.divContainer').click(function(){
         $(this).toggleClass('selected');
     });
+    $('#button').click(function(){
+        if(confirm("Press enter to delete")){
+            $('.selected').remove();
+        }
+    });
 });
