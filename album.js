d074a3af71f78a7267d5da0c0855a1f81131e79d
Ramon Fabrega <ramonfabrega0@gmail.com>
2014-04-05 16:34:15 -0500

    extracted js to functions

---
+++ album.js
@@ -0,0 +1,24 @@
+function makeList(){
+	for(var i = 1; i <= 639; i++){
+		var divTag = document.createElement("div");
+		divTag.id = i;
+		divTag.className = "divContainer";
+		document.body.appendChild(divTag);
+		var pTg = document.createElement("p");
+		pTg.setAttribute("align", "center");
+		pTg.className = "text";
+		pTg.innerHTML = (i);
+		document.getElementById(i).appendChild(pTg);
+	}
+}
+
+function bindClickEvents(){
+	$('.divContainer').click(function(){
+		$(this).toggleClass('selected');
+	});
+	$('#button').click(function(){
+		if(confirm("Press enter to delete")){
+			('.selected').remove();
+		}
+	});
+}
