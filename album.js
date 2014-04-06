e2e6d118f6604dbf5e9b1e60ca9d8f47e751fcd5
Ramon Fabrega <ramonfabrega0@gmail.com>
2014-04-06 16:40:35 -0500

    formatted code. no changes in functionality

--- album.js
+++ album.js
@@ -1,15 +1,27 @@
-function makeList(){
-	for(var i = 1; i <= 639; i++){
-		var divTag = document.createElement("div");
-		divTag.id = i;
-		divTag.className = "divContainer";
-		document.body.appendChild(divTag);
-		var pTg = document.createElement("p");
-		pTg.setAttribute("align", "center");
-		pTg.className = "text";
-		pTg.innerHTML = (i);
-		document.getElementById(i).appendChild(pTg);
+var TOTAL_STICKERS = 639;
+var selectedIds = [];
+var missingUserStickers = (function(){
+	var arr = [];
+	for(var i = 1; i <= TOTAL_STICKERS; i++){
+		arr.push(i);
 	}
+	return arr;
+})();
+
+function makeList(){
+	missingUserStickers.map(makeListItem);
+}
+
+function makeListItem (itemNumber){
+	var divTag = document.createElement("div");
+	divTag.id = itemNumber;
+	divTag.className = "divContainer";
+	document.body.appendChild(divTag);
+	var pTg = document.createElement("p");
+	pTg.setAttribute("align", "center");
+	pTg.className = "text";
+	pTg.innerHTML = (itemNumber);
+	document.getElementById(itemNumber).appendChild(pTg);
 }
 
 function bindClickEvents(){
@@ -18,7 +30,26 @@ function bindClickEvents(){
 	});
 	$('#button').click(function(){
 		if(confirm("Press enter to delete")){
-			$('.selected').remove();
+			deleteSelected();			
 		}
 	});
+}
+
+function deleteSelected(){
+	var selected = $('.selected');
+	for(var i = 0; i < selected.length; i++){
+		var num = Number(selected[i].id)
+		selectedIds.push(num);
+	}
+	selectedIds.map(removeFromMissingUserStickers);
+	selected.remove();
+	localStorage.setItem('missingUserStickers', JSON.stringify(missingUserStickers));
+}
+
+function removeFromMissingUserStickers (number){
+	var toDelete = missingUserStickers.indexOf(number);
+	console.log(toDelete);
+	if (toDelete > -1){
+		missingUserStickers.splice(toDelete, 1);
+	}
 }
